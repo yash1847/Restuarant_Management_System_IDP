860238a85c90618e917158b93979af0fcdc965b6
2370110 <2370110@LTIN434663>
2024-12-17 11:31:37 +0530

    Add project files.

---
+++ Restuarant_Management_System_IDP/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Restuarant_Management_System_IDP.Models;
+using System.Diagnostics;
+
+namespace Restuarant_Management_System_IDP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Restuarant_Management_System_IDP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Restuarant_Management_System_IDP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
