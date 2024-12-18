0a7231a4b1b928e059103e64e1f37e7eca47ee80
Giriboyana <2370110@cognizant.com>
2024-12-18 11:57:44 +0530

    latest

---
+++ Restuarant_Management_System_IDP/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restuarant_Management_System_IDP.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
