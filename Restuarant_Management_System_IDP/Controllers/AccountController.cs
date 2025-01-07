9c02944cae9969375802d35b8a78604fa0c31d41
Giriboyana <2370110@cognizant.com>
2025-01-07 16:33:15 +0530

    getting up to date

--- Restuarant_Management_System_IDP/Controllers/AccountController.cs
+++ Restuarant_Management_System_IDP/Controllers/AccountController.cs
@@ -1,16 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Restuarant_Management_System_IDP.Models;
 
 namespace Restuarant_Management_System_IDP.Controllers
 {
     public class AccountController : Controller
     {
+        [HttpGet]
         public IActionResult Login()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(IFormCollection frmc)
+        {  
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
         {
             return View();
         }
-        public IActionResult Signup()
+
+        public IActionResult Usertable()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(IFormCollection frmc) 
+        {
+            if (!ModelState.IsValid)
+            {
+                //invalid
+                return View();
+            }
+
+            string customerId = frmc["User.CustomerId"];
+            return Content(customerId);
         }
     }
 }
