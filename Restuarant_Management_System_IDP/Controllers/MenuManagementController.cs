2b2eb7945eb2b180acd574c257a8a85d6ca2b41a
2370110 <2370110@LTIN434663>
2025-01-22 15:14:51 +0530

    menumanagement

---
+++ Restuarant_Management_System_IDP/Controllers/MenuManagementController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Restuarant_Management_System_IDP.Models;
+
+namespace Restuarant_Management_System_IDP.Controllers
+{
+    [Authorize(Roles = SD.Admin)]
+    public class MenuManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
